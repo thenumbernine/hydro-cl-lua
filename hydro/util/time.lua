081ba25fc0cd71b29716d35dab74b8083eee6ee6
Christopher Moore <christopher.e.moore@gmail.com>
2022-03-15 13:48:18 +0600

    oops, can't believe I forgot to switch this to the ext version

--- hydro/util/time.lua
+++ hydro/util/time.lua
@@ -1,25 +1,2 @@
-require 'ffi.c.sys.time'
-local ffi = require 'ffi'
-
-local getTime
-if ffi.os == 'Windows' then
-	-- in linux this is the live time, or something other than the actual time
-	getTime = os.clock
-else
-	local gettimeofday_tv = ffi.new'struct timeval[1]'
-	function getTime()
-		local results = ffi.C.gettimeofday(gettimeofday_tv, nil)
-		return tonumber(gettimeofday_tv[0].tv_sec) + tonumber(gettimeofday_tv[0].tv_usec) / 1000000
-	end
-end
-
-local function time(name, cb)
-	print(name..'...')
-	local startTime = getTime()
-	local result = table.pack(cb())
-	local endTime = getTime()
-	print('...done '..name..' ('..(endTime - startTime)..'s)')
-	return table.unpack(result, 1, result.n)
-end
-
-return {time, getTime}
+local timer = require 'ext.timer'
+return {timer.timer, timer.getTime}
