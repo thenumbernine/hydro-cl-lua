a1d14ed8806248109ffe677e82e3e67ace4ab853
Christopher Moore <christopher.e.moore@gmail.com>
2024-08-29 19:10:16 -0700

    switching to new sys

--- hydro/draw/2d_graph.lua
+++ hydro/draw/2d_graph.lua
@@ -82,28 +82,36 @@ function Draw2DGraph:showDisplayVar(var)
 
 	local sceneObj = solver.draw2DGraphSceneObj
 
-	-- 3 components per vertex
-	local vertexes = sceneObj.attrs.vertex.buffer.vec
-
 	local step = math.max(1, self.step)
 	local numX = math.floor((tonumber(solver.gridSize.x) - 2 * solver.numGhost + 1) / step)
 	local numY = math.floor((tonumber(solver.gridSize.y) - 2 * solver.numGhost + 1) / step)
 	-- 2 vtxs per tristrip
 	local numVertexes = 2 * numX * numY
 
-	if #vertexes ~= numVertexes then
-		vertexes:resize(numVertexes)
-
-		local vtxbuf = sceneObj.attrs.vertex.buffer
-		vtxbuf.data = vertexes.v
-		vtxbuf:bind()
-			:setData{
-				size = numVertexes * ffi.sizeof'vec3f_t',
-				data = vertexes.v,
-			}
-		sceneObj.geometry.count = numVertexes
+-- [[ new sys ... half the speed of above
+	local vertexes = sceneObj.attrs.vertex.buffer:beginUpdate()
+	vertexes:resize(numVertexes)
+	local index = 0
+	for j=0,numY-2 do
+		for i=0,numX-1 do
+			local x = i * step
+			for jofs=0,1 do
+				local y = (j + jofs) * step
+				--[=[ goes horribly slow
+				vertexes:emplace_back():set(x,y,0)
+				--]=]
+				-- [=[ just as fast as old sys
+				local v = vertexes.v[index]
+				v.x = x
+				v.y = y
+				v.z = 0--app.displayFixedZ
+				index = index + 1
+				--]=]
+			end
+		end
 	end
-
+	sceneObj.attrs.vertex.buffer:endUpdate()
+--]]
 
 	local shader = solver.graphShader
 	local uniforms = shader.uniforms
@@ -121,25 +129,9 @@ function Draw2DGraph:showDisplayVar(var)
 	-- TODO where to specify using the heatmap gradient vs using the variable/solver color
 	gl.glUniform3f(uniforms.color.loc, (#app.solvers > 1 and solver or var).color:unpack())
 
-	local index = 0
-	for j=0,numY-2 do
-		for i=0,numX-1 do
-			local x = i * step
-			for jofs=0,1 do
-				local y = (j + jofs) * step
-				local v = vertexes.v[index]
-				v.x = x
-				v.y = y
-				v.z = 0--app.displayFixedZ
-				index = index + 1
-			end
-		end
-	end
-
 	solver.draw2DGraphSceneObj.attrs.vertex.buffer
 		:bind()
 		:updateData()
-
 
 	solver.draw2DGraphSceneObj:enableAndSetAttrs()
 
